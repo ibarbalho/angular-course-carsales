46eaf58aa7b773dac5c63b7da72c21a09f87c8ef
Ittalo Barbalho <ittalo.renan@gmail.com>
2024-04-03 15:39:18 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-course-carsales';
+  title = 'Bem vindo ao Curso de Frontend com Angular';
 }
